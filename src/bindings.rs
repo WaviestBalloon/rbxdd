0bd92c69db203786fa14038a7b7faac1541f4682
WaviestBalloon <31796727+WaviestBalloon@users.noreply.github.com>
2025-04-05 15:26:37 +0100

    Update Studio bindings

--- src/bindings.rs
+++ src/bindings.rs
@@ -4,7 +4,7 @@ pub static PLAYER_EXTRACT_BINDINGS: [(&str, &str); 22] = [
 	("shaders.zip", "shaders/"),
 	("ssl.zip", "ssl/"),
 	("WebView2.zip", ""),
-	("WebView2RuntimeInstaller.zip", "WebView2RuntimeInstaller/"), // (Not usual required)
+	("WebView2RuntimeInstaller.zip", "WebView2RuntimeInstaller/"),
 
 	("content-avatar.zip", "content/avatar/"),
 	("content-configs.zip", "content/configs/"),
@@ -25,45 +25,40 @@ pub static PLAYER_EXTRACT_BINDINGS: [(&str, &str); 22] = [
 	("extracontent-textures.zip", "ExtraContent/textures/"),
 	("extracontent-places.zip", "ExtraContent/places/")
 ];
-pub static STUDIO_EXTRACT_BINDINGS: [(&str, &str); 34] = [
-	("RobloxStudio.zip", ""),
-	("RibbonConfig.zip", "RibbonConfig/"),
-	("redist.zip", ""),
-	("Libraries.zip", ""),
-	("LibrariesQt5.zip", ""),
-	("WebView2.zip", ""),
-	("WebView2RuntimeInstaller.zip", "WebView2RuntimeInstaller/"), // (Not usual required)
-
-	("shaders.zip", "shaders/"),
-	("ssl.zip", "ssl/"),
-	("Qml.zip", "Qml/"),
-	("Plugins.zip", "Plugins/"),
-	("StudioFonts.zip", "StudioFonts/"),
-	("BuiltInPlugins.zip", "BuiltInPlugins/"),
+pub static STUDIO_EXTRACT_BINDINGS: [(&str, &str); 33] = [
 	("ApplicationConfig.zip", "ApplicationConfig/"),
-	("BuiltInStandalonePlugins.zip", "BuiltInStandalonePlugins/"),
-
-	("content-qt_translations.zip", "content/qt_translations/"),
-	("content-sky.zip", "content/sky/"),
-	("content-fonts.zip", "content/fonts/"),
+	("redist.zip", ""),
+	("RobloxStudio.zip", ""),
+	("Libraries.zip", ""),
 	("content-avatar.zip", "content/avatar/"),
-	("content-models.zip", "content/models/"),
-	("content-sounds.zip", "content/sounds/"),
 	("content-configs.zip", "content/configs/"),
-	("content-api-docs.zip", "content/api_docs/"),
-	("content-studio_svg_textures.zip", "content/studio_svg_textures/"),
-	("content-terrain.zip", "PlatformContent/pc/terrain/"),
+	("content-fonts.zip", "content/fonts/"),
+	("content-sky.zip", "content/sky/"),
+	("content-sounds.zip", "content/sounds/"),
 	("content-textures2.zip", "content/textures/"),
+	("content-studio_svg_textures.zip", "content/studio_svg_textures/"),
+	("content-models.zip", "content/models/"),
 	("content-textures3.zip", "PlatformContent/pc/textures/"),
-
+	("content-terrain.zip", "PlatformContent/pc/terrain/"),
 	("content-platform-fonts.zip", "PlatformContent/pc/fonts/"),
 	("content-platform-dictionaries.zip", "PlatformContent/pc/shared_compression_dictionaries/"),
-
-	("extracontent-translations.zip", "ExtraContent/translations/"),
-	("extracontent-luapackages.zip", "ExtraContent/LuaPackages/"),
-	("extracontent-textures.zip", "ExtraContent/textures/"),
+	("content-qt_translations.zip", "content/qt_translations/"),
+	("content-api-docs.zip", "content/api_docs/"),
 	("extracontent-scripts.zip", "ExtraContent/scripts/"),
-	("extracontent-models.zip", "ExtraContent/models/")
+	("extracontent-luapackages.zip", "ExtraContent/LuaPackages/"),
+	("extracontent-translations.zip", "ExtraContent/translations/"),
+	("extracontent-models.zip", "ExtraContent/models/"),
+	("extracontent-textures.zip", "ExtraContent/textures/"),
+	("shaders.zip", "shaders/"),
+	("BuiltInPlugins.zip", "BuiltInPlugins/"),
+	("BuiltInStandalonePlugins.zip", "BuiltInStandalonePlugins/"),
+	("LibrariesQt5.zip", ""),
+	("Plugins.zip", "Plugins/"),
+	("RibbonConfig.zip", "RibbonConfig/"),
+	("StudioFonts.zip", "StudioFonts/"),
+	("ssl.zip", "ssl/"),
+	("WebView2.zip", ""),
+	("WebView2RuntimeInstaller.zip", "WebView2RuntimeInstaller/")
 ];
 
 pub static LATEST_VERSION_PLAYER: &str = "https://clientsettings.roblox.com/v2/client-version/WindowsPlayer";
